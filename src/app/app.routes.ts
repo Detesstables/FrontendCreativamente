f4aaea4d9a6ad4a2241edad6f85d63acfd496c01
Detesstables <jorgeluisrebazamicha@gmail.com>
2024-02-18 02:16:59 -0300

    Pequeño avance

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './components/views/inicio/inicio.component';
+import { FormularioCategoriasComponent } from './components/views/formulario-categorias/formulario-categorias.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'inicio',
+        title: 'Pagina principal',
+        component: InicioComponent
+    },
+    {
+        path:'',
+        redirectTo: '/inicio',
+        pathMatch: 'full'
+    },
+    {
+        path: 'formularioCategoria',
+        title: 'Formulario de categorias',
+        component: FormularioCategoriasComponent
+    }
+];
---
+++ src/app/components/views/formulario-categorias/formulario-categorias.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from '../../shared/navbar/navbar.component';
+
+@Component({
+  selector: 'app-formulario-categorias',
+  standalone: true,
+  imports: [NavbarComponent],
+  templateUrl: './formulario-categorias.component.html',
+  styleUrl: './formulario-categorias.component.css'
+})
+export class FormularioCategoriasComponent {
+
+}
